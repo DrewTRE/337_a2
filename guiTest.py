6d80cbf6b8640f37bca1eae5c5d87b78fc3612e2
DrewTRE <43226138+DrewTRE@users.noreply.github.com>
2019-03-03 04:01:01 -0800

    Formatting Update
    Updated the format back to normal since things are in the right place now.

--- guiTest.py
+++ guiTest.py
@@ -10,7 +10,7 @@ window = Tk()
 window.title("Wallet")
 
 # Window Size
-window.geometry('550x150')
+window.geometry('550x130')
 tab_control = ttk.Notebook(window)
 tab_control.pack()
 
@@ -22,98 +22,48 @@ tab2 = ttk.Frame(tab_control)
 tab1.grid_columnconfigure(1, weight = 2)
 tab2.grid_columnconfigure(1, weight = 2)
 
-# Tab Labels
-tab_control.add(tab1, 	text 	= 'Recieve')
-tab_control.add(tab2, 	text 	= 'Send')
-tab_control.pack(		expand 	= 1, 
-				 		fill 	= 'both'
-				 		)
+########################### Tab Labels ########################################
+tab_control.add(tab1, text = 'Recieve')
+tab_control.add(tab2, text = 'Send')
+tab_control.pack(fill = 'both')
 
-# Text in the main window
-label 	= Label(window, 
-				text 	= 'Wallet Amount',
-				padx 	= 5, 
-				pady 	= 0,
-				)
+########################### Text in the main window ###########################
+label = Label(window, text = 'Wallet Amount', padx = 5, pady = 0)
 label.pack()
 
-labela 	= Label(window, 
-				text 	= Wallet,
-				padx 	= 5, 
-				pady 	= 0,
-				)
+labela = Label(window, text = Wallet, padx = 5, pady = 0)
 labela.pack()
 
 # Tab 1 
-label1 	= Label(tab1, 
-				text 	= 'Enter Wallet ID', 
-				padx 	= 5, 
-				pady 	= 5)
-label1.grid(	column	= 0, 
-				row 	= 0, 
-				sticky 	= W
-				)
+label1 = Label(tab1, text 	= 'Enter Wallet ID', padx = 5, pady = 5)
+label1.grid(column = 0, row = 0, sticky = W)
 
-label1a 	= Label(tab1, 
-				text 	= 'Enter EMD', 
-				padx 	= 5, 
-				pady 	= 5)
-label1a.grid(	column	= 0, 
-				row 	= 1, 
-				sticky 	= W
-				)
+label1a = Label(tab1, text = 'Enter EMD', padx 	= 5, pady = 5)
+label1a.grid(column	= 0, row = 1, sticky = W)
 
 
 # Tab 2
-label2 	= Label(tab2, 
-				text 	= 'Enter Wallet ID', 
-				padx 	= 5, 
-				pady 	= 5)
-label2.grid(	column	= 0, 
-				row 	= 0, 
-				sticky 	= W
-				)
+label2 	= Label(tab2, text 	= 'Enter Wallet ID', padx = 5, pady = 5)
+label2.grid(column	= 0, row = 0, sticky = W)
 
-label2a 	= Label(tab2, 
-				text 	= 'Send Amount', 
-				padx 	= 5, 
-				pady 	= 5)
-label2a.grid(	column	= 0, 
-				row 	= 1, 
-				sticky 	= W
-				)
+label2a = Label(tab2, text 	= 'Send Amount', padx = 5, pady = 5)
+label2a.grid(column	= 0, row = 1, sticky = W)
 
 
-# Entry
+########################### Entry #############################################
 # Tab 1
-idEntry1 = Entry(	tab1, 
-					width 	= 75
-					)
-idEntry1.grid( 		row 	= 0,
-					column	= 1
-					)
-
-hexEntry = Entry(	tab1, 
-					width 	= 75
-					)
-hexEntry.grid( 		row 	= 1,
-					column	= 1
-					)
-
+idEntry1 = Entry(tab1, width = 75)
+idEntry1.grid(row = 0, column = 1)
+hexEntry = Entry(tab1, width = 75)
+hexEntry.grid(row = 1, column = 1)
 # Tab 2
-idEntry2 = Entry(	tab2, 
-					width 	= 75)
-idEntry2.grid( 		row 	= 0,
-					column	= 1
-					)
+idEntry2 = Entry(tab2, width  = 75)
+idEntry2.grid(row = 0, column = 1)
 
-amountEntry = Entry(tab2,
-					width	= 75)
-amountEntry.grid( 	row 	= 1,
-					column	= 1
-					)
+amountEntry = Entry(tab2, width	= 75)
+amountEntry.grid(row = 1, column = 1)
 
-# Buttons
+########################### Buttons ###########################################
 # Output when the button is clicked.
 # clickedRecieve and clickedSend should both update the Wallet total which is on it's
 # own window, so they both can update the global variable Wallet. 
@@ -130,52 +80,17 @@ def sync():
 	#Add Sync Function
 
 # Tab 1
-button_sync1 = Button(tab1, 	text 	= 'Sync', 
-								command = sync,
-								padx 	= 0, 
-								pady 	= 0)
-button_sync1.grid(				column 	= 2, 
-								row 	= 0,
-								padx 	= 5, 
-								pady 	= 0,
-								sticky 	= EW
-								)
+button_sync1 = Button(tab1, text = 'Sync', command = sync, padx = 0, pady = 0)
+button_sync1.grid(column = 2, row = 0, padx = 5, pady = 0,sticky = EW)
 
-
-button_receive = Button(tab1, 	text 	= 'Receive', 
-								command = clickedSend,
-								padx 	= 0, 
-								pady 	= 0)
-button_receive.grid(			column 	= 2, 
-								row 	= 1,
-								padx 	= 5, 
-								pady 	= 0,
-								ipadx	= 1, 
-								sticky 	= EW
-								)
+button_receive = Button(tab1, text 	= 'Receive', command = clickedSend, padx = 0, pady = 0)
+button_receive.grid(column = 2, row = 1,padx = 5, pady = 0,ipadx= 1, sticky = EW)
 
 # Tab 2
-button_sync2 = Button(tab2, 	text 	= 'Sync',
-								command = sync,
-								padx 	= 0, 
-								pady 	= 0)
-button_sync2.grid(				column 	= 2, 
-								row 	= 0,
-								padx 	= 5, 
-								pady 	= 0,
-								ipadx 	= 9,
-								sticky 	= EW
-								)
+button_sync2 = Button(tab2, text = 'Sync', command = sync, padx = 0, pady = 0)
+button_sync2.grid(column = 2, row = 0, padx = 5, pady 	= 0, ipadx = 9, sticky = EW)
 
-button_send2 = Button(tab2, 	text 	= 'Send',
-								command = clickedSend,
-								padx 	= 0, 
-								pady 	= 0)
-button_send2.grid(				column 	= 2, 
-								row 	= 1,
-								padx 	= 5, 
-								pady 	= 0,
-								sticky 	= EW
-								)
+button_send2 = Button(tab2, text = 'Send', command = clickedSend, padx = 0, pady = 0)
+button_send2.grid(column = 2, row = 1, padx = 5, pady = 0, sticky = EW)
 
 window.mainloop()
